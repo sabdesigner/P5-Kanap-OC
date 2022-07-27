85207faa44bcdc275aa1b68bf8bdc1320aa04eb3
Sabrina Ambre Biller <contact@sabrinambre.fr>
2022-07-27 17:39:55 +0200

    Récuperation des infos Id pour affichage du produit

--- front/js/product.js
+++ front/js/product.js
@@ -10,9 +10,11 @@ let product = [];
 // Asynchrone, pour aller chercher dans l'API + mot clef await
 async function init() {
   
-// Attend le retour de user avant de buildHTML
+// Attend le retour de user avant de buildProduct
   product = await getProduct();
   console.log(product);
+ 
+  buildProduct(product);
 
   if (product.length > 0) {
     console.log(product);
@@ -22,7 +24,6 @@ async function init() {
       // document.createElement("div").innerHTML = "Désolé il n'y a plus de produits";
   }
 }
-
 
 function getProduct() {
     return (
@@ -38,13 +39,38 @@ function getProduct() {
               alert("Nous n'avons pas réussi à afficher nos canapés. Avez vous bien lancé le serveur local (Port 3000) ? Si le problème persiste prenons contact");
               return error;
       })    
-    );
+    ); 
   }
 
+function buildProduct(product) {
+// Image
+let productImg = document.createElement("img");
+document.querySelector(".item__img").appendChild(productImg);
+productImg.src = product.imageUrl;
+productImg.alt = product.altTxt;
 
+// Nom
+let productName = document.getElementById('title');
+productName.innerHTML = product.name;
+
+// Description
+let productDescription = document.getElementById("description");
+productDescription.innerHTML = product.description;
+
+// Prix
+let productPrice = document.getElementById("price");
+productPrice.innerHTML = `${product.price}€`;
+
+// Colors
+let select = document.getElementById("colors");
+// Pour chaque couleur, crée une option et met la dans le select
+product.colors.forEach((color) => {
+  let option = document.createElement("option");
+  option.value = color;
+  option.innerHTML = color;
+  select.appendChild(option);
+});
+
+}
 //Lancement du site, on fait tout
 init();
-
-
-
-
